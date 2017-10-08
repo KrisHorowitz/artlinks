6bc21ab8a101a879077c46925823540d1a429b14
Michael Rubino <msrubino@gmail.com>
2017-10-08 15:24:19 -0400

    Convert all the visual stuff into paper objects

--- js/script.js
+++ js/script.js
@@ -2,28 +2,33 @@
 var $win = $(window),
     w = 0,h = 0,
     rgb = [],
+    red, green, blue,
     getWidth = function() {
         w = $win.width();
         h = $win.height();
     };
 
-var canvas, canvasCtx;
-$(function() {
-    // canvas = document.getElementById("canvas");
-    // canvasCtx = canvas.getContext("2d");
-});
+var zeroPad = function(hexString) {
+    switch (hexString.length) {
+        case 0:
+            hexString = "00";
+            break;
+        case 1:
+            hexString = "0" + hexString;
+            break;
+    }
+    return hexString;
+}
 
 $win.resize(getWidth).mousemove(function(e) {
-    // if (canvas === undefined || canvasCtx === undefined) return;
-    // var CONST_BLUE = 150;
-    // var red = Math.round(e.pageX/w * 175).toString(16);
-    // var green = Math.round(e.pageY/h * 175).toString(16);
-    // var blue = CONST_BLUE.toString(16);
-    // rgb = [
-    //     red,
-    //     green,
-    //     blue
-    // ];
-    // var color = "#" + rgb.join('');
-    // $(document.body).css('background', color);
+    var CONST_BLUE = 150;
+    console.log(Math.round(e.pageX/w * 175));
+    red = Math.round(e.pageX/w * 175).toString(16);
+    green = Math.round(e.pageY/h * 175).toString(16);
+    blue = CONST_BLUE.toString(16);
+    rgb = [
+        zeroPad(red),
+        zeroPad(green),
+        zeroPad(blue)
+    ];
 }).resize();
